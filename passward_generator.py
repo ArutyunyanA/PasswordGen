e14419d4c2e8f6037c1ae54add9cfbb7605709fe
ArutyunyanA <126784114+ArutyunyanA@users.noreply.github.com>
2023-11-27 19:46:42 +0100

    Update passward_generator.py

--- passward_generator.py
+++ passward_generator.py
@@ -8,12 +8,14 @@ def main(user_input):
     print('//////////////////////////////////////////////////////////////////////////////')
     print()
     while user_input != 'exit':
-        user_input = int(input('Please, choice length of passwords between 10 or 15\nto terminate program please type -> exit\n'))
+        user_input = input('Please, choice length of passwords between 10 or 15\nto terminate program please type -> exit\n')
         alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation    
-        if user_input == 10:
+        if user_input in '10':
             print(''.join(choice(alphabet) for char in range(10)))
-        elif user_input == 15:
+        elif user_input in '15':
             print(''.join(choice(alphabet) for char in range(15)))
+        elif user_input in ('exit'):
+            print('By, Have fun!')
         else:
             print('Error, please try to restart programm\nand insert the valid numbers!')
 
